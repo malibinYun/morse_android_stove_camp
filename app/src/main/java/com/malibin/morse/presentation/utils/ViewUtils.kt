98f7024fbd951d8ecdf6563c2bed53c90007491b
Malibin <nightmare73@naver.com>
2021-01-25 15:06:56 +0900

    [Feat] 버튼, 이메일 인증 관련 뷰 작성

--- app/src/main/java/com/malibin/morse/presentation/utils/ViewUtils.kt
+++ app/src/main/java/com/malibin/morse/presentation/utils/ViewUtils.kt
@@ -1,18 +1,26 @@
 package com.malibin.morse.presentation.utils
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.databinding.BindingAdapter
 
 /**
  * Created By Malibin
  * on 1월 11, 2021
  */
 
-fun Context.showToast(message: String) {
+fun Context.showToast(message: String?) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-fun Context.showToast(@StringRes stringResId: Int) {
-    Toast.makeText(this, stringResId, Toast.LENGTH_SHORT).show()
+fun Context.showToast(@StringRes stringResId: Int?) {
+    if (stringResId == null) showToast(null as? String?)
+    else Toast.makeText(this, stringResId, Toast.LENGTH_SHORT).show()
+}
+
+@BindingAdapter("isEnabled")
+fun bindingIsEnabled(view: View, isEnabled: Boolean?) {
+    view.isEnabled = isEnabled ?: false
 }
